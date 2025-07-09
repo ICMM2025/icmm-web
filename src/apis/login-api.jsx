f0b4b34271a15eb6bab643f8f9974bad50efe074
BesRoom <info.besroom@gmail.com>
2025-07-09 16:11:14 +0700

    Fix small bug

--- src/apis/login-api.jsx
+++ src/apis/login-api.jsx
@@ -4,3 +4,6 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const loginApi = async (body) =>
   await axios.post(`${baseUrl}/auth/login`, body);
+
+export const testDBApi = async (body) =>
+  await axios.get(`${baseUrl}/test`, body);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import Button from "../components/main/Button";
 import Input from "../components/main/Input";
 import { LoginIcon } from "../icons/mainIcon";
-import { loginApi } from "../apis/login-api";
+import { loginApi, testDBApi } from "../apis/login-api";
 import useModalStore from "../stores/modal-store";
 import useUserStore from "../stores/user-store";
 import { useNavigate } from "react-router-dom";
@@ -12,6 +12,11 @@ import LanguageSelect from "../components/main/LanguageSelect";
 function Login() {
   const { t } = useTranslation();
   const navigate = useNavigate();
+
+  const setErrTxt = useModalStore((state) => state.setErrTxt);
+  const setIsErrorModalOpen = useModalStore(
+    (state) => state.setIsErrorModalOpen
+  );
   const setIsLoadingModalOpen = useModalStore(
     (state) => state.setIsLoadingModalOpen
   );
@@ -26,7 +31,13 @@ function Login() {
     setInput((prv) => ({ ...prv, [e.target.name]: e.target.value }));
   };
 
-  const hdlLogin = async () => {
+  const hdlError = (err) => {
+    setErrTxt(err);
+    setIsErrorModalOpen(true);
+  };
+
+  const hdlLogin = async (e) => {
+    e.preventDefault();
     setIsLoadingModalOpen(true);
     try {
       const res = await loginApi(input);
@@ -35,6 +46,20 @@ function Login() {
       navigate("/");
     } catch (err) {
       console.log(err?.response?.data?.msg || err.message);
+      hdlError(err?.response?.data?.msg || err.message);
+    } finally {
+      setIsLoadingModalOpen(false);
+    }
+  };
+
+  const hdlTestDB = async () => {
+    setIsLoadingModalOpen(true);
+    try {
+      const res = await testDBApi();
+      hdlError(res.data.test.test);
+    } catch (err) {
+      console.log(err?.response?.data?.msg || err.message);
+      hdlError(err?.response?.data?.msg || err.message);
     } finally {
       setIsLoadingModalOpen(false);
     }
@@ -46,19 +71,32 @@ function Login() {
       <div className="w-full min-w-[360px] max-w-[1024px] min-h-svh mx-auto bg-m-light flex flex-col">
         {/* main */}
         <div className="w-[360px] mx-auto mt-8 border border-m-line/50 rounded-m p-4 flex justify-center items-center gap-1">
-          <p>{t("admin")}</p>
-          <Input
-            type="password"
-            size="2"
-            placeholder={t("password")}
-            value={input.password}
-            onChange={hdlInput}
-            name="password"
-          />
-          <Button lbl={t("login")} Icon={LoginIcon} onClick={hdlLogin} />
+          <form
+            onSubmit={hdlLogin}
+            className="flex justify-center items-center gap-1"
+          >
+            <p>{t("admin")}</p>
+            <Input
+              type="password"
+              size="2"
+              placeholder={t("password")}
+              value={input.password}
+              onChange={hdlInput}
+              name="password"
+            />
+            <Button
+              lbl={t("login")}
+              Icon={LoginIcon}
+              onClick={hdlLogin}
+              type="submit"
+            />
+          </form>
         </div>
         <div className="w-full flex justify-center mt-4">
           <LanguageSelect />
+        </div>
+        <div className="w-full flex justify-center py-4">
+          <Button lbl="Test DB" size="2" onClick={hdlTestDB} />
         </div>
       </div>
     </>
